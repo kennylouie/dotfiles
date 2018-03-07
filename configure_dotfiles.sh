d3f2f85d7a3f20606c905397a57d1444c39a2caf
Kenny Louie <kennylouie@protonmail.ch>
2018-03-06 20:23:52 -0800

    Changed listing of buffers to <C-w>a from l since <C-w>l is the default for moving between splits.

--- configure_dotfiles.sh
+++ configure_dotfiles.sh
@@ -305,7 +305,7 @@ nnoremap <C-w>< :vertical resize -5<CR>
 nnoremap <C-w>> :vertical resize +5<CR>
 
 "buffer listing
-nnoremap <C-w>l :ls<CR>
+nnoremap <C-w>a :ls<CR>
 
 "buffer deletion
 nnoremap <C-w>q :ls<CR>
